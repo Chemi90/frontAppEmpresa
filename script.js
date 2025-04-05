381014df6b486d7f2d02840aec252a8c043dc17a
Jose Miguel <xemiruiz@gmail.com>
2025-04-05 21:41:44 +0200

    añadimos archivos para crear la pwa

--- script.js
+++ script.js
@@ -887,4 +887,17 @@ document.addEventListener('DOMContentLoaded', function() {
       facturaCancelBtn.style.display = "inline-block";
     }
   });
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('ServiceWorker registrado con éxito:', registration.scope);
+        })
+        .catch(err => {
+          console.log('Error al registrar el ServiceWorker:', err);
+        });
+    });
+  }
+  
 });
